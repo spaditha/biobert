e31006830b7456b015519927588aab3d94ab05d4
spaditha <48846489+spaditha@users.noreply.github.com>
2021-04-29 13:01:53 -0400

    Create Merge.py

---
+++ Merge.py
@@ -0,0 +1,15 @@
+pwd
+
+%cd '/content/sample_data'
+
+f1data = f2data = "" 
+ 
+with open('BioASQform_BioASQ-answer_factoid_testset4.json') as f1: 
+  f1data = f1.read() 
+with open('BioASQform_BioASQ-answer_yesno_testset4.json') as f2: 
+  f2data = f2.read() 
+  
+  f1data += "\n"
+f1data += f2data
+with open ('Merge.json', 'a') as f3: 
+  f3.write(f1data)
