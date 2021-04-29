f595c190a467d9b745b8ac31b12d4b238b20c3a8
spaditha <48846489+spaditha@users.noreply.github.com>
2021-04-29 19:25:52 -0400

    Update Merge.py

--- Merge.py
+++ Merge.py
@@ -9,12 +9,14 @@ with open('BioASQform_BioASQ-answer_factoid_testset4.json') as f1:
 with open('BioASQform_BioASQ-answer_yesno_testset4.json') as f2: 
   f2data = f2.read() 
 with open('BioASQform_BioASQ-answer_list_testset4.json') as f2: 
-  f2data = f2.read() 
+  f3data = f3.read() 
   
   f1data += "\n"
 f1data += f2data
-with open ('Merge.json', 'a') as f3: 
-  f3.write(f1data)
+f1data += "\n"
+f1data += f3data
+with open ('Merge.json', 'a') as f4: 
+  f4.write(f1data)
 
   
  
